6717df6a3c941a24700f9fb165f6400055c80b2f
oliullah05 <mdolihasan@gmail.com>
2023-10-06 17:28:27 +0600

    add and delete oparation done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express")
+const express = require('express')
+const cors = require('cors');
 const app = express();
-
 const port = process.env.port || 5000 ;
 
+app.use(cors())
+app.use(express.json())
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://Toys-Kingdom:aBlvDos75WTOlUch@cluster0.iono61s.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,22 +44,38 @@ run().catch(console.dir);
 app.get("/",(req,res)=>{
     res.send("server is working ")
 })
-app.get("/all-toys",(req,res)=>{
-    
+app.get("/alltoys",async(req,res)=>{
+    const allToys = await allToysCollection.find().toArray()
+    res.send(allToys)
+})
+
+
+app.get("/mytoys/:email",async(req,res)=>{
+const getParams = req.params.email;
+console.log(getParams);
+  const query = { user_email:getParams};
+  const options = {};
+  const allToys = await allToysCollection.find(query,options).toArray()
+  res.send(allToys)
 })
 
 
 
+app.post("/addtoy",async(req,res)=>{
+ const toyData =req.body;
+  // console.log(toyData);
+  const result = await allToysCollection.insertOne(toyData);
+  res.send(result)
+})
 
+app.delete("/delete/:id",async(req,res)=>{
+const getId =req.params.id;
 
-
-
-
-
-
-
-
-
+ const query = { _id:new ObjectId(getId) }
+//  console.log(query);
+ const result = await allToysCollection.deleteOne(query);
+ res.send(result)
+})
 
 
 
